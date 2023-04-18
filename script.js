3ccfb47e9b5d58a939fddb04e5410712000721df
ZCrooks <zachthedevguy@gmail.com>
2023-04-18 10:18:20 -0400

    created base Pseudo Code for script

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// DATA TO DISPLAY
-// 
+// DATA TO DISPLAY IN CAROUSEL
+// title
+// objectName
+// primaryImage
+// department
+// period
+// artistDisplayName
+// country
 
-// 
+
+// PSEUDO CODE
+// 1. User selects a department from dropdown Menu (value)
+    // use 'Search' call for all objects with a certain department Id (S)
+    // then use 'Object' call to Filter each Object ID and for each create
+// 2. Upon clicking 'Search', a new page (gallery) will load with a hover carousel (ul - li) of 10 RANDOMLY selected artpieces from the department that the user selected
+// 3. For each art piece (li) feature the vital info (IMAGE, artist if available, department, culture, time period, make)
+// 4. Use Reset button to go back and make another selection
+ 
